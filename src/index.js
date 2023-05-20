bfbcce34e5b3f47d9fff7210b5af8d34ccf01968
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-05-20 11:30:02 +0300

    Create index.js and score.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import './styles.css';
+import "./styles.css";
+import { createGame } from "./api.js";
+import { refreshScores, submitScore } from "./scores.js";
+
+const gameId = "ABC123";
+
+const createAndLoadGame = async (name) => {
+  try {
+    await createGame(name);
+    await refreshScores(gameId);
+  } catch (error) {
+    throw new Error("Failed to create and load game");
+  }
+};
+
+const clearInputs = () => {
+  document.getElementById("name").value = "";
+  document.getElementById("score").value = "";
+};
+
+document.getElementById("refresh-button").addEventListener("click", () => {
+  refreshScores(gameId);
+});
+
+document
+  .getElementById("add-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const user = document.getElementById("name").value;
+    const score = document.getElementById("score").value;
+    await submitScore(gameId, user, score);
+    clearInputs();
+  });
+
+// Call the createAndLoadGame function to initiate the game creation and score loading
+createAndLoadGame("Abdelaziz");
---
+++ src/scores.js
@@ -0,0 +1,30 @@
+import { getScores, addScore } from './api.js';
+
+const displayScores = (scores) => {
+  const scoresList = document.getElementById('scores-list');
+  scoresList.innerHTML = '';
+
+  scores.forEach((score, index) => {
+    const item = document.createElement('li');
+    item.innerHTML = `${index + 1}. ${score.user}: ${score.score}`;
+    scoresList.appendChild(item);
+  });
+};
+
+export const refreshScores = async (gameId) => {
+  try {
+    const scores = await getScores(gameId);
+    displayScores(scores);
+  } catch (error) {
+    throw new Error('Failed to refresh scores');
+  }
+};
+
+export const submitScore = async (gameId, user, score) => {
+  try {
+    await addScore(gameId, user, score);
+    await refreshScores(gameId);
+  } catch (error) {
+    throw new Error('Failed to submit score');
+  }
+};
